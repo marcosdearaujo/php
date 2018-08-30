275d070c0fc1c7153e4f1e4be1a1c6462e2dd6d1
marcosdearaujo <37121840+marcosdearaujo@users.noreply.github.com>
2018-08-30 11:07:02 -0300

    Alterei o olá mundo 123 e Hcode.

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 
-<<<<<<< HEAD
-echo "Olá Mundo 456!";
-
-echo "Olá Udemy!";
-=======
-echo "Olá Mundo 123 !";
+echo "Olá Mundo 123!";
 
 echo "Olá Hcode!";
->>>>>>> 2351269ab566a673ae97e6c9b4b213f79618c93e
 
 ?>
